f4c862754404367148221dd3839c109a5ae02978
naveen <knaveenkr500@gmail.com>
2026-02-22 15:16:23 +0530

    created exception class for handling duplicate email

---
+++ patient-service/src/main/java/com/pm/patient_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.pm.patient_service.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException ex) {
+       Map<String, String> errors=new HashMap<>();
+
+       ex.getBindingResult().getFieldErrors().forEach(
+               error -> errors.put(error.getField(), error.getDefaultMessage())
+       );
+
+       return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(DuplicateEmailException.class)
+    public ResponseEntity<Map<String, String>> handleDuplicateEmailException(DuplicateEmailException ex) {
+        Map<String, String> error = new HashMap<>();
+        log.warn("Email address already exists: {}", ex.getMessage());
+        error.put("message", "Email address already exists");
+        error.put("status", "400");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+}
